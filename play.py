f2f32ce0ac6a91804b35c022d4eb78ed250760df
itkr <itkrst@gmail.com>
2018-02-08 03:05:47 +0900

    init

---
+++ play.py
@@ -0,0 +1,38 @@
+from time import sleep
+from selenium import webdriver
+
+DRIVER_PATH = './chromedriver_mac64'
+URL = 'http://radiko.jp/#!/live/TBS'
+
+
+class Radio(object):
+
+    def __init__(self, driver_path, url):
+        self.driver = webdriver.Chrome(driver_path)
+        self.driver.get(url)
+
+    def play_or_stop(self):
+        self._play_button.click()
+
+    def exit(self):
+        self.driver.close()
+        self.driver.quit()
+
+    @property
+    def _play_button(self):
+        return self.driver.find_element_by_class_name('btn--primary')
+
+    def __enter__(self):
+        self.play_or_stop()
+
+    def __exit__(self, exc_type, exc_value, traceback):
+        self.exit()
+
+
+def main():
+    with Radio(DRIVER_PATH, URL):
+        sleep(60 * 60)
+
+
+if __name__ == '__main__':
+    main()
